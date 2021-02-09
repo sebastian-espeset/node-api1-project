9f35e981b65e11ba7350c3a0301f70a97007d866
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-08 23:26:33 -0600

    server up and running

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+//define the server
 const server = require('./api/server');
 
+//import express framework for routing etc.
+const express = require("express");
+
+//import the generate function from shortId
+const generate = require("shortid").generate;
+
+//define app as express, don't know why...
+const app = express();
 const port = 5000;
 
+
 // START YOUR SERVER HERE
+app.listen(port, ()=>{
+    console.log(`server running at port: ${port}`)
+})
