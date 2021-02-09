5669d67dadac392040385ebf5e43beb37c42a999
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-08 23:45:38 -0600

    successfully posting new users and returning the new array

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const generate = require("shortid").generate;
 
 //define app as express, don't know why...
 const app = express();
+
+//app.use??? Dont know why...
+app.use(express.json());
+
 const port = 5000;
 
 //dummy data
@@ -36,9 +40,25 @@ const users=[
 ]
 
 // START YOUR SERVER HERE
+//axios.get
 app.get('/users',(req, res)=>{
     res.status(200).json(users)
 });
+//axios.post
+app.post('/users',(req,res)=>{
+    //destructure the body requirement
+    const { name, bio} = req.body;
+    //if they ARE NOT included in the post, send error message
+    if(!name || !bio){
+        res.status(400).json({message:`Error, must include both name and bio`})
+    }
+    //if name and bio are included, post to server and return the new server, include status 201 i.e created a resource successfully
+    else{
+        const newUser={id:generate(),name, bio};
+        users.push(newUser);
+        res.status(201).json(users);
+    }
+})
 
 app.listen(port, ()=>{
     console.log(`server running at port: ${port}`)
